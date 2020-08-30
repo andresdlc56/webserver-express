0ccc45685a4403b724547dfc20e1019770d459ed
Andrés <andresdlc56@gmail.com>
2020-08-30 14:20:01 -0400

    probando

--- server.js
+++ server.js
@@ -3,18 +3,16 @@ const app = express();
 
 const hbs = require('hbs');
 
-app.use(express.static(__dirname + '/public'));
-
-//Express HBS
-hbs.registerPartials(__dirname + '/views/parciales');
-app.set('view engine', 'hbs');
-
 //hbs helpers
 require('./hbs/helpers');
 
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/public'));
 
+//Express HBS
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
 
 app.get('/', (req, res) => {
     res.render('home', {
